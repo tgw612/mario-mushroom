4d83b1a05fac155ff056b9581dabfb71e3e672ff
tgw <453424815@qq.com>
2018-10-28 09:11:25 +0800

    jvm

---
+++ jvm/v1/code/java/jvmgo_java/ch01/src/test/java/com/github/jvmgo/ArgsTest.java
@@ -0,0 +1,25 @@
+package com.github.jvmgo;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ArgsTest {
+
+    @Test
+    public void test() {
+        assertTrue(com.github.jvmgo.Args.parse(new String[]{"-?"}).helpFlag);
+        assertTrue(com.github.jvmgo.Args.parse(new String[]{"-help"}).helpFlag);
+        assertTrue(com.github.jvmgo.Args.parse(new String[]{"-version"}).versionFlag);
+        assertFalse(com.github.jvmgo.Args.parse(new String[]{"-cp"}).ok);
+        assertFalse(com.github.jvmgo.Args.parse(new String[]{"-classpath"}).ok);
+        assertEquals("foo.jar", com.github.jvmgo.Args.parse(new String[]{"-cp", "foo.jar"}).classpath);
+        assertEquals("foo.jar", com.github.jvmgo.Args.parse(new String[]{"-classpath", "foo.jar"}).classpath);
+        assertEquals(Arrays.asList("Main", "foo"), com.github.jvmgo.Args.parse(new String[]{"Main", "foo"}).mainClassAndArgs);
+    }
+
+}
