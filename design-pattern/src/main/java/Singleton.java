889396694df31cf3566597b46a2f3392680e9b30
tgw612 <tgw612@hotmail.com>
2018-11-03 16:03:25 +0800

    design

---
+++ design-pattern/src/main/java/Singleton.java
@@ -0,0 +1,13 @@
+public class Singleton {
+    private static class SingletonHolder {
+        public static Singleton instatnce = new Singleton();
+    }
+
+    private Singleton() {
+
+    }
+
+    public static Singleton newInstatnce() {
+        return SingletonHolder.instatnce;
+    }
+}
