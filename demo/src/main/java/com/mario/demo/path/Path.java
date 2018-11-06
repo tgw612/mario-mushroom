0d7c502f5b58d92bf88ffb4c86e19b6c61815623
tgw612 <tgw612@hotmail.com>
2018-11-06 15:33:16 +0800

    log

---
+++ demo/src/main/java/com/mario/demo/path/Path.java
@@ -0,0 +1,12 @@
+package com.mario.demo.path;
+
+import java.net.URISyntaxException;
+
+public class Path {
+    public static void main(String[] args) throws URISyntaxException {
+
+        String path = Thread.currentThread().getContextClassLoader().getResource("")
+                .toURI().getPath().replaceAll("%20", " ");
+        System.out.println(path);
+    }
+}
