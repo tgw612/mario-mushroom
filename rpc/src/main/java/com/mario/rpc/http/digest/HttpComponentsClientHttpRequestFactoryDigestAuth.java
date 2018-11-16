8154cee2d8d4454dad22a196a5ad24ced44a3699
tgw612 <tgw612@hotmail.com>
2018-11-16 13:48:53 +0800

    nice

---
+++ rpc/src/main/java/com/mario/rpc/http/digest/HttpComponentsClientHttpRequestFactoryDigestAuth.java
@@ -0,0 +1,51 @@
+package com.mario.rpc.http.digest;
+
+import org.apache.http.HttpHost;
+import org.apache.http.client.AuthCache;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.auth.DigestScheme;
+import org.apache.http.impl.client.BasicAuthCache;
+import org.apache.http.protocol.BasicHttpContext;
+import org.junit.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.apache.http.protocol.HttpContext;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+/**
+ * https://mp.weixin.qq.com/s/Czn0E5dl2qVKqkLG27OVfg
+ */
+public class HttpComponentsClientHttpRequestFactoryDigestAuth extends HttpComponentsClientHttpRequestFactory {
+    RestTemplate restTemplate;
+    HttpHost host;
+
+    @Test
+    public  void main(String[] args) {
+        String uri ="http://localhost:8080/spring-security-rest-digest-auth/api/foos/1";
+//        ResponseEntity<Foo>entity
+    }
+    public HttpComponentsClientHttpRequestFactoryDigestAuth(HttpClient httpClient, HttpHost host) {
+        super(httpClient);
+        this.host = host;
+    }
+
+    @Override
+    protected HttpContext createHttpContext(HttpMethod httpMethod, URI uri) {
+        return createHttpContext();
+    }
+
+    private HttpContext createHttpContext() {
+        AuthCache authCache =new BasicAuthCache();
+        DigestScheme digestScheme =new DigestScheme();
+        digestScheme.overrideParamter("realm","custom realm name");
+        authCache.put(host,digestScheme);
+        BasicHttpContext basicHttpContext =new BasicHttpContext();
+        basicHttpContext.setAttribute(ClientContext.AUTH_CACHE,authCache);
+        return basicHttpContext;
+    }
+}
