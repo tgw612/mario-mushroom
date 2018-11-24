386372e12a60e0e695adf4b474fcb5e5653f4447
tgw <453424815@qq.com>
2018-11-24 19:42:29 +0800

    nice

---
+++ rabbitmq/rabbit_consumer/src/main/java/com/mario/rabbitmq/HelloApplication.java
@@ -0,0 +1,15 @@
+package com.mario.rabbitmq;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableRabbit
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+
+}
---
+++ rabbitmq/rabbit_consumer/src/test/java/HelloApplicationTest.java
@@ -0,0 +1,29 @@
+import com.mario.rabbitmq.HelloApplication;
+import com.mario.rabbitmq.Sender;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HelloApplication.class)
+public class HelloApplicationTest {
+    @Autowired
+    private Sender sender;
+
+    @Test
+    public void hello() throws Exception {
+        sender.send();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
