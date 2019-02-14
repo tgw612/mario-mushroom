d54335efc89f2a8be9a20514769bc1d3287af4f4
tgw <453424815@qq.com>
2019-02-14 20:57:29 +0800

    nice

---
+++ jvm/src/main/java/com.mario.jvm/SimpleHeap.java
@@ -0,0 +1,20 @@
+package com.mario.jvm;
+
+public class SimpleHeap {
+    private int id;
+
+    public SimpleHeap(int id) {
+        this.id = id;
+    }
+
+    public static void main(String[] args) {
+        SimpleHeap s1 = new SimpleHeap(1);
+        SimpleHeap s2 = new SimpleHeap(2);
+        s1.show();
+        s2.show();
+    }
+
+    public void show() {
+        System.out.println("my id is" + id);
+    }
+}
